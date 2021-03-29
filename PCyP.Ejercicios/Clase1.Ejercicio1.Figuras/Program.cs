468db650f3a046236f7e893e85730023c4bd6327
nahuel arias <ariasnahuel8@gmail.com>
2021-03-29 19:56:26 -0300

    entendiendo git hub
    creo que le agarro la mano

--- PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Program.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figuras/Program.cs
@@ -10,6 +10,15 @@ namespace Clase1.Ejercicio1.Figuras
     {
         static void Main(string[] args)
         {
+            // Realizar un programa capaz de calcular el área y perímetro de un cuadrado, rectángulo y triángulo. 
+            //Todos los datos necesarios para su funcionamiento se ingresan por teclado.
+
+
+            Console.WriteLine("hola");
+
+
+
+
         }
     }
 }
---
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Figurasgeometricas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+          
+
+        }
+    }
+}
