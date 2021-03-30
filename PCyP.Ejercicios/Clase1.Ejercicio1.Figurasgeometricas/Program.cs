a078d95fc977f59002c1be7cd69b02b30b644318
nahuel arias <ariasnahuel8@gmail.com>
2021-03-30 17:06:18 -0300

    terminado
    esta funcional

--- PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/Program.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/Program.cs
@@ -29,6 +29,7 @@ namespace Clase1.Ejercicio1.Figurasgeometricas
                 {
                     case 1:
 
+                        //CUADRADO
                         
                         Console.WriteLine("ingrese un lado");
                         l = Console.ReadLine();
@@ -44,20 +45,62 @@ namespace Clase1.Ejercicio1.Figurasgeometricas
                         break;
 
                     case 2:
+                        int base2 = 0;
+                        int altura = 0;
+                        //RECTANGULO
+                        Console.WriteLine("ingrese la base");
+                        l = Console.ReadLine();
+                        base2 = int.Parse(l);
 
+                        Console.WriteLine("ingrese la altura");
+                        l = Console.ReadLine();
+                        altura = int.Parse(l);
+
+                        Console.WriteLine("PERIMETRO");
+                        Console.WriteLine(f1.PerimetroRectangulo(base2,altura));
+
+                        Console.WriteLine("AREA");
+                        Console.WriteLine(f1.areadelRectangulo(base2, altura));
 
                         break;
 
                     case 3:
+                        //TRIANGULO
+                        int lado2 = 0;
+                        int lado3 = 0;
+
+                        Console.WriteLine("ingrese la base");
+                        l = Console.ReadLine();
+                        base2 = int.Parse(l);
+
+                        Console.WriteLine("ingrese la altura");
+                        l = Console.ReadLine();
+                        altura = int.Parse(l);
+
+                        Console.WriteLine("ingrese un lado");
+                        l = Console.ReadLine();
+                        lado = int.Parse(l);
+                        Console.WriteLine("ingrese un lado 2");
+                        l = Console.ReadLine();
+                        lado2 = int.Parse(l);
+                        Console.WriteLine("ingrese un lado 3");
+                        l = Console.ReadLine();
+                        lado3 = int.Parse(l);
 
 
+                        Console.WriteLine("AREA");
+                        Console.WriteLine(f1.areadelTriangulo(base2, altura));
+
+                        Console.WriteLine("PERIMETRO");
+
+                        Console.WriteLine(f1.PerimetroTriangulo(lado, lado2, lado3));
 
                         break;
                 }
             } while (opcion!=0);
 
 
-            Console.ReadKey();
+            
 
 
 
--- PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/figura.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/figura.cs
@@ -28,9 +28,31 @@ namespace Clase1.Ejercicio1.Figurasgeometricas
             return lado *= lado;
         }
 
+        public int PerimetroRectangulo(int base2, int altura)
+        {
 
+            return (2*base2)+(2*altura);
 
+        }
+        public int PerimetroTriangulo(int lado1, int lado2, int lado3)
+       
+        {
+            return lado1+lado2+lado3;
+        }
 
+        public int areadelRectangulo(int base2, int altura)
+        {
+
+            return base2 * altura;
+
+        }
+
+        public int areadelTriangulo(int base2, int altura)
+        {
+            return (base2 * altura)/2;
+        }
+
+       
     }
 
    
