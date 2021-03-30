8e0b77b2ed7a0d5016d46f153f0ef004e9d7f510
nahuel arias <ariasnahuel8@gmail.com>
2021-03-30 14:39:47 -0300

    definir estructura main
    se definio la estructura de el main

--- PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/Program.cs
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/Program.cs
@@ -10,9 +10,44 @@ namespace Clase1.Ejercicio1.Figurasgeometricas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
-          
+            //Realizar un programa capaz de calcular el área y perímetro de un cuadrado, rectángulo y triángulo. 
+            //Todos los datos necesarios para su funcionamiento se ingresan por teclado.
+
+
+            int opcion = 0;
+            do
+            {
+                
+                Console.WriteLine(" 1--para area y perimetro de un cuadrado ");
+                Console.WriteLine(" 2--para area y perimetro de un rectangulo ");
+                Console.WriteLine(" 3--para area y perimetro de un triangulo");
+                Console.WriteLine(" 0--para salir");
+
+                switch (opcion)
+                {
+                    case 1:
+
+
+                        break;
+
+                    case 2:
+
+
+                        break;
+
+                    case 3:
+
+
+
+                        break;
+                }
+            } while (opcion!=0);
+
+
+
+
+
 
         }
     }
---
+++ PCyP.Ejercicios/Clase1.Ejercicio1.Figurasgeometricas/figura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1.Ejercicio1.Figurasgeometricas
+{
+    class figura
+    {
+        private List<int> Figura;
+
+    }
+
+    public void setFigura(int valor, int pos)
+    {
+
+    }
+
+
+    public List<int> getFIgura()
+    {
+        return (figura);
+    }
+
+}
