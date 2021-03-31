9ed5335ac80695426f718b37a9e86b7d6ea6ef7c
nahuel arias <ariasnahuel8@gmail.com>
2021-03-31 20:47:38 -0300

    desarrolando imprimir
    se desarrolla la funcion imprimir

--- PCyP.Ejercicios/ConsoleApplication3/Program.cs
+++ PCyP.Ejercicios/ConsoleApplication3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace ConsoleApplication3
     {
         static void Main(string[] args)
         {
+            string[] lineas= File.ReadAllLines("./files/vial.csv");
+
+
+            foreach(var linea in lineas)
+            {
+                var valores = linea.Split(',');
+                Console.WriteLine(" NUMERO DE HOJA: "+ valores[0]
+                    +" TIPO OBJETO: "+ valores[1]
+                    +" IDENTIFICADOR TRAMO: "+ valores[2]
+                    +" TIPO CAMINO: "+valores[3]
+                    +" LONGITUD: "+ valores[4]);
+                Console.WriteLine("");
+            }
+
+
         }
     }
 }
+
